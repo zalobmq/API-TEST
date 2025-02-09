eca628b9713906d6fac898ad967f043a3fdcfb88
GonzaloBmq <gonzalobretones-moraquero@MacBook-Pro-de-Gonzalo.local>
2025-02-09 13:59:39 +0100

    Curso nodeJs

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const http = require('http');
+const http = require('http')
 
-const server = http.createServer((req , res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('HOLA MUNDO');
-    res.end();
-});
+const processRequest = (req, res) => {
+  res.setHeader('Conten-Type', 'text/plain; charset=utf-8')
+  if (req.url === '/') {
+    res.statusCode = 200 // OK
+    res.end('<h1> Bienvenido </h1>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // OK
+    res.end('<h1> Mi contacto </h1>')
+  } else {
+    res.statusCode = 404 // Error 404
+    res.end('<h1> Error 404 </h1>')
+  }
+}
 
-const port = 3000;
-server.listen (port, () => {
-    console.log('Server running')
-});
+const server = http.createServer(processRequest)
+
+const desiredPort = 3000
+server.listen(desiredPort, () => {
+  console.log(`Server running on port -> http://localhost:${desiredPort}`)
+})
